26b388c621b91bb5323bba589b152688c9bede89
Sam-MARTis <samanthmartis@gmail.com>
2024-02-26 20:21:52 +0530

    Trying to fix discontinuous atan2 function

--- main.js
+++ main.js
@@ -15,22 +15,20 @@ const setupFunction = () => {
   globalCanvas.style.width = globalCanvas.width + "px";
   globalCanvas.style.height = globalCanvas.height + "px";
 
-    context = globalCanvas.getContext("2d");
-    allBoxes = new Boxes(context, width, height)
+  context = globalCanvas.getContext("2d");
+  allBoxes = new Boxes(context, width, height);
 
   window.addEventListener("mousemove", handleMouseMove);
 };
 
 class Box {
-    constructor(
-        {
-            context = context,
-            width = 30 + Math.random() * 20,
-            height = 30 + Math.random() * 20,
-            posX = 100 + Math.random() * 500,
-            posY = 100 + Math.random() * 500
-        }
-  ) {
+  constructor({
+    context = context,
+    width = 30 + Math.random() * 20,
+    height = 30 + Math.random() * 20,
+    posX = 100 + Math.random() * 500,
+    posY = 100 + Math.random() * 500,
+  }) {
     this.cx = posX;
     this.cy = posY;
     this.width = width;
@@ -73,66 +71,125 @@ class Box {
     this.updateShadowCorners(fromX, fromY);
     // console.log("bottomShadowCorner", this.bottomShadowCorner);
     // console.log("topShadowCorner", this.topShadowCorner);
-    this.drawFromCorner(
-      this.bottomShadowCorner,
-      this.topShadowCorner,
-      this.minAngle,
-      this.maxAngle
-    );
+    // this.drawFromCorner(
+    //   this.bottomShadowCorner,
+    //   this.topShadowCorner,
+    //   this.minAngle,
+    //   this.maxAngle
+    // );
+    // this.updateShadowCorners(fromX, fromY);
     // console.log("Drawing shadow");
   }
-  drawFromCorner(bottomCorner, topCorner, minAngle, maxAngle) {
-    let maxTrigValue = Math.max(
-      Math.abs(1 / Math.sin(minAngle)),
-      Math.abs(1 / Math.cos(minAngle)),
-      Math.abs(1 / Math.sin(maxAngle)),
-      Math.abs(1 / Math.cos(maxAngle))
-    );
-    let length = Math.max(width * maxTrigValue, height * maxTrigValue);
-    //   let length = 100
-    this.context.beginPath();
-    this.context.strokeStyle = "rgba(0,0,0,0.1)";
-    let bottomStartPosX = bottomCorner.x;
 
-    let bottomStartPosY = bottomCorner.y;
-    let topStartPosX = topCorner.x;
-    let topStartPosY = topCorner.y;
-    let topEndPosX = topStartPosX + Math.cos(maxAngle) * length;
-    let topEndPosY = topStartPosY - Math.sin(maxAngle) * length;
-    let bottomEndPosX = bottomStartPosX + Math.cos(minAngle) * length;
-    let bottomEndPosY = bottomStartPosY - Math.sin(minAngle) * length;
+  // drawFromCorner(bottomCorner, topCorner, minAngle, maxAngle) {
+  //   let maxTrigValue = Math.max(
+  //     Math.abs(1 / Math.sin(minAngle)),
+  //     Math.abs(1 / Math.cos(minAngle)),
+  //     Math.abs(1 / Math.sin(maxAngle)),
+  //     Math.abs(1 / Math.cos(maxAngle))
+  //   );
+  //   let length = Math.max(width * maxTrigValue, height * maxTrigValue);
+  //   //   let length = 100
+  //   this.context.beginPath();
+  //   this.context.strokeStyle = "rgba(0,0,0,0.1)";
+  //   let bottomStartPosX = bottomCorner.x;
 
-    this.context.moveTo(topStartPosX, topStartPosY);
-    this.context.lineTo(bottomStartPosX, bottomStartPosY);
-    this.context.lineTo(bottomEndPosX, bottomEndPosY);
-    this.context.lineTo(bottomEndPosX, topEndPosY);
-    this.context.lineTo(topEndPosX, topEndPosY);
-    this.context.lineTo(topStartPosX, topStartPosY);
-    this.context.closePath();
-    this.context.fillStyle = "rgba(0,0,0,0.2";
-    this.context.fill();
-    this.context.stroke();
-    this.drawSelf();
-  }
+  //   let bottomStartPosY = bottomCorner.y;
+  //   let topStartPosX = topCorner.x;
+  //   let topStartPosY = topCorner.y;
+  //   let topEndPosX = topStartPosX + Math.cos(maxAngle) * length;
+  //   let topEndPosY = topStartPosY - Math.sin(maxAngle) * length;
+  //   let bottomEndPosX = bottomStartPosX + Math.cos(minAngle) * length;
+  //   let bottomEndPosY = bottomStartPosY - Math.sin(minAngle) * length;
+
+  //   this.context.moveTo(topStartPosX, topStartPosY);
+  //   this.context.lineTo(bottomStartPosX, bottomStartPosY);
+  //   this.context.lineTo(bottomEndPosX, bottomEndPosY);
+  //   this.context.lineTo(bottomEndPosX, topEndPosY);
+  //   this.context.lineTo(topEndPosX, topEndPosY);
+  //   this.context.lineTo(topStartPosX, topStartPosY);
+  //   this.context.closePath();
+  //   this.context.fillStyle = "rgba(0,0,0,1";
+  //   this.context.fill();
+  //   this.context.stroke();
+  //   this.drawSelf();
+  // }
   updateShadowCorners(fromX, fromY) {
     this.minAngle = Infinity;
     this.maxAngle = -Infinity;
     Object.values(this.corners).forEach((corner) => {
-      //   console.log("Corner x: " + corner.x);
-      //   console.log("Corner y: " + corner.y);
       let delX = corner.x - fromX;
       let delY = fromY - corner.y;
       let angle = Math.atan2(delY, delX);
-      //   console.log(corner, angle);
-      if (angle < this.minAngle) {
-        this.minAngle = angle;
-        this.bottomShadowCorner = corner;
-      }
-      if (angle > this.maxAngle) {
-        this.maxAngle = angle;
-        this.topShadowCorner = corner;
-      }
+      corner.theta = angle;
+      // console.log(corner, angle);
+      // if (corner.theta < this.minAngle) {
+      //   this.minAngle = corner.theta;
+      //   this.bottomShadowCorner = corner;
+      // }
+      // if (corner.theta > this.maxAngle) {
+      //   this.maxAngle = corner.theta;
+      //   this.topShadowCorner = corner;
+      // }
+      // this.pickShadowCorners()
+
+
+
+      this.context.beginPath();
+      Object.values(this.corners).forEach((corner) => {
+        this.context.moveTo(corner.x, corner.y);
+        let angle = corner.theta;
+        let maxTrigFactor = Math.max(1 / Math.sin(angle), 1 / Math.cos(angle));
+        let length = Math.max(width * maxTrigFactor, height * maxTrigFactor);
+        this.context.lineTo(
+          corner.x + Math.abs(Math.cos(angle) * length )* Math.sign(delX),
+          corner.y - Math.abs(Math.sin(angle) * length )* Math.sign(delY)
+        );
+        this.context.stroke();
+        // this.context.closePath();
+        this.context.fillStyle = "rgba(0,0,0,1";
+        this.context.fill();
+        this.context.stroke();
+        this.drawSelf();
+      });
     });
+  }
+  pickShadowCorners() {
+    let cornerAngles = [];
+    Object.values(this.corners).forEach((corner) => {
+      cornerAngles.push(corner.theta);
+    });
+    console.log(Math.min(...cornerAngles), Math.max(...cornerAngles));
+
+    if (Math.max(...cornerAngles) - Math.min(...cornerAngles) < Math.PI/2) {
+      console.log("If statement");
+      this.minAngle = Math.min(...cornerAngles);
+      this.maxAngle = Math.max(...cornerAngles);
+      Object.values(this.corners).forEach((corner) => {
+        if (corner.theta == this.minAngle) {
+          this.bottomShadowCorner = corner;
+        }
+        if (corner.theta == this.maxAngle) {
+          this.topShadowCorner = corner;
+        }
+      });
+    } else {
+      console.log("Else statement");
+      cornerAngles.forEach((angle) => {
+        if (angle < 0) angle = 6.28 - angle;
+      });
+      this.minAngle = Math.min(...cornerAngles);
+      this.maxAngle = Math.max(...cornerAngles);
+      console.log(this.minAngle, this.maxAngle);
+      Object.values(this.corners).forEach((corner) => {
+        if (corner.theta == this.minAngle) {
+          this.bottomShadowCorner = corner;
+        }
+        if (corner.theta == this.maxAngle) {
+          this.topShadowCorner = corner;
+        }
+      });
+    }
   }
 }
 
@@ -165,27 +222,23 @@ class Boxes {
     this.boxes = [];
     this.width = width;
     this.height = height;
-    this.createBoxes(10);
+    this.createBoxes(1);
   }
 
   createBoxes(n) {
-      for (let i = 0; i < Math.floor(Math.sqrt(n))+1; i++) {
-          for (let j = 0; j < Math.ceil(Math.sqrt(n)); j++) {
-              if (Math.random() < 0.3) continue;
-              let box = new Box({
-                  context: this.context,
+    for (let i = 0; i < Math.floor(Math.sqrt(n)) + 1; i++) {
+      for (let j = 0; j < Math.ceil(Math.sqrt(n)); j++) {
+        if (Math.random() < 0.003) continue;
+        let box = new Box({
+          context: this.context,
 
-                  posX:
-                      i* this.width/ Math.sqrt(n) +
-                      Math.random()*200 -100,
-                  posY:
-                      (j * this.height) / Math.sqrt(n) +
-                      Math.random()*200 -100,
-              });
-              box.drawSelf();
-              this.boxes.push(box);
-          }
+          posX: (i * this.width) / Math.sqrt(n) + Math.random() * 200 - 100,
+          posY: (j * this.height) / Math.sqrt(n) + Math.random() * 200 - 100,
+        });
+        box.drawSelf();
+        this.boxes.push(box);
       }
+    }
   }
   updateBoxes(x, y) {
     this.boxes.forEach((box) => {
